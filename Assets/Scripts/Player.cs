61ac2d1dfa097ace3de3cf429bb466730674011a
AhhhhYe <hiddendreams3112@gmail.com>
2024-01-23 14:39:35 +0800

    Finished Creating the Game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
+    public scorescript ss;
+    public GameObject obstacle;
+    public Rigidbody rb;
+    public float jump;
     private Animation thisAnimation;
 
     void Start()
@@ -15,6 +19,25 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump");
+            rb.velocity = Vector3.zero;
+            rb.AddForce(new Vector3(0, jump, 0));
             thisAnimation.Play();
+        }
+        if (this.transform.position.y <= -3.7f)
+        {
+            Debug.Log("Lose");
+            SceneManager.LoadScene("GameLose");
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == ("wall"))
+        {
+            Debug.Log("Lose");
+            SceneManager.LoadScene("GameLose");
+        }
     }
 }
---
+++ Assets/Scripts/scorescript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class scorescript : MonoBehaviour
+{
+    public float score;
+    public Text Scoretext;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            Scoretext.GetComponent<Text>().text = ("Score: " + score);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "wall")
+        {
+            Debug.Log("Score");
+            score++;
+            Scoretext.GetComponent<Text>().text = ("Score: " + score);
+        }
+    }
+    private void FixedUpdate()
+    {
+        GameObject.FindGameObjectsWithTag("wall");
+    }
+}
